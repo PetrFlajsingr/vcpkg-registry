dc1d04695413ab1133f7c3d50cc5418e73f26bcf
PetrFlajsingr <flajsingr@gmail.com>
2022-04-08 19:30:42 +0200

    Update portfile.cmake

--- ports/pf-mainloop/portfile.cmake
+++ ports/pf-mainloop/portfile.cmake
@@ -1,9 +1,9 @@
 vcpkg_from_github(
 	OUT_SOURCE_PATH SOURCE_PATH
 	REPO PetrFlajsingr/pf_mainloop
-	HEAD_REF main 
-	REF aea503cdf2a684ac946e9551a68bf5bcab2b2dbb
-	SHA512 31b6cef9ed2d97c03eec4f5198fc842fe75ad8de4ddc7d8fdeaecbbc1ad41e79364f905dd2518b6cf6d456b0e898a277e5a6e1b927cc31e65b22c62654488602
+	HEAD_REF vcpkg_dependencies 
+	REF a83d938afcfb248637946982e60b3834b01a4d1a
+	SHA512 0
 )
 
 vcpkg_configure_cmake(
