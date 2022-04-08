49e6b6ad4d6d6bff500844a12b92456024efc25e
PetrFlajsingr <flajsingr@gmail.com>
2022-04-08 18:12:55 +0200

    add pf_glfw

---
+++ ports/pf-glfw/portfile.cmake
@@ -0,0 +1,14 @@
+vcpkg_from_github(
+	OUT_SOURCE_PATH SOURCE_PATH
+	REPO PetrFlajsingr/pf_glfw
+	HEAD_REF vcpkg_dependencies 
+	REF 122cda09552569daae8770ae33dd17eb9e1ec8ab
+	SHA512 0
+)
+
+vcpkg_configure_cmake(
+	SOURCE_PATH "${SOURCE_PATH}"
+	PREFER_NINJA
+)
+vcpkg_install_cmake()
+vcpkg_fixup_cmake_targets()
