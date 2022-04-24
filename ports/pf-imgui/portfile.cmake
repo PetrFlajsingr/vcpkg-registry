6b9b5387fff3d080e5cc011abdb36e1ff59cadc3
PetrFlajsingr <flajsingr@gmail.com>
2022-04-24 13:49:53 +0200

    Update portfile.cmake

--- ports/pf-imgui/portfile.cmake
+++ ports/pf-imgui/portfile.cmake
@@ -3,7 +3,7 @@ vcpkg_from_github(
 	REPO PetrFlajsingr/pf_imgui
 	HEAD_REF master 
 	REF b31deb50d56b1e82c2f9469fc383a9e2b355e866
-	SHA512 0
+	SHA512 cc087ceb6c5b508bd4e3568e25bb9e4d54c13c0841826dccea226cb6f1cd4b1b5010ef02e395092550cee6a80c0c02992223c4d422d7e75bf7ef6269d3224b38
 )
 
 vcpkg_configure_cmake(
