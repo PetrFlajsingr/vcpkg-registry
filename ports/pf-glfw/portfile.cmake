cce9ed4d848281f2bc1690127a81f743c27afa82
PetrFlajsingr <flajsingr@gmail.com>
2022-09-24 20:20:12 +0200

    Update pf-glfw

--- ports/pf-glfw/portfile.cmake
+++ ports/pf-glfw/portfile.cmake
@@ -2,8 +2,8 @@ vcpkg_from_github(
 	OUT_SOURCE_PATH SOURCE_PATH
 	REPO PetrFlajsingr/pf_glfw
 	HEAD_REF master 
-	REF 1bd038d5b55e32a8ca3f1974c8726610a552115e
-	SHA512 a86b2a148c85dfd011d259fef5fa5f6dbd80bdb891481605acaaede36524a06d749b8400b18bd29d08fd5b551f004ec222c025bb07e1b7570bd510f21d2efc53
+	REF 467a233c0d35765b605fe3a86f357503a4a9945d
+	SHA512 0
 )
 
 vcpkg_configure_cmake(
