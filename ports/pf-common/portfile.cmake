0639c23285f458daed0d31f439f47dc450561bd3
PetrFlajsingr <flajsingr@gmail.com>
2023-02-11 13:12:02 +0100

    Update portfile.cmake

--- ports/pf-common/portfile.cmake
+++ ports/pf-common/portfile.cmake
@@ -2,8 +2,8 @@ vcpkg_from_github(
 	OUT_SOURCE_PATH SOURCE_PATH
 	REPO PetrFlajsingr/pf_common
 	HEAD_REF master 
-	REF 735c1b1deae82e5ad1f59ff6e16d812f965b4990
-	SHA512 ee86bc67c390068e19a8bece8643f87a22e9ff42ed73cbb46ed2f0d83f3d8b9f71d971bb25159a668cc45a180bb5e5cdd1c5ae35db4eccc41970dddf9f2245ae
+	REF c7eec13b89cda0b820e8afda6d54d210d4498a1e
+	SHA512 0
 )
 
 vcpkg_configure_cmake(
